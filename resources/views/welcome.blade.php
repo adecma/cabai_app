adc50165826fa81348e474be9b76ca3c6cdadb01
Ade Prast <adecma18@gmail.com>
2017-05-15 21:26:20 +0800

    modified welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,13 @@
 
 @section('content')
     <div class="panel panel-success">
-        <div class="panel-heading">Welcome, guest!</div>
+        <div class="panel-heading">Welcome, Bro!</div>
 
         <div class="panel-body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi labore voluptate aspernatur, non sequi nemo quisquam quae assumenda eius consequuntur quod reiciendis illum temporibus accusantium officia animi laudantium aut, natus!
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi labore voluptate aspernatur, non sequi nemo quisquam quae assumenda eius consequuntur quod reiciendis illum temporibus accusantium officia animi laudantium aut, natus!
+			<br><br>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima, quam eveniet quia officiis hic? Explicabo doloribus modi optio magni quis. At consequuntur explicabo recusandae, tenetur provident quod quasi quae, quia.
+            </p>
         </div>
     </div>
 @endsection
